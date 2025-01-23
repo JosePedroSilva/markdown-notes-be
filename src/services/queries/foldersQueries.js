0b1b023f8c399f8b4e8f06f9d94f914b2facebad
Jose Pedro Silva <josepedro88@live.com.pt>
2025-01-23 16:31:03 +0100

    Implement folder deletion logic with cascading updates to notes and add transaction handling

--- src/services/queries/foldersQueries.js
+++ src/services/queries/foldersQueries.js
@@ -1,12 +1,22 @@
-const { Folder } = require('../../models');
+const { Folder, Note } = require('../../models');
+const { Op } = require('sequelize');
 
 exports.createFolder = async (folderId, name, userId, parentFolderId) => {
-  console.log('Creating folder', { folderId, name, userId, parentFolderId });
+  const existingFolder = await Folder.findOne({
+    where: {
+      id: parentFolderId,
+      user_id: userId,
+      deleted: false,
+    },
+  });
+
+  console.log('existing folder', existingFolder);
+
   const folder = await Folder.create({
     id: folderId,
     name,
     userId: userId,
-    parentFolderId: parentFolderId,
+    parentFolderId: existingFolder ? parentFolderId : null,
   });
 
   return folder;
@@ -17,7 +27,6 @@ exports.updateFolder = async (folderId, name, userId, parentFolderId) => {
   if (name) updateFields.name = name;
   if (parentFolderId) updateFields.parentFolderId = parentFolderId;
 
-  console.log('Updating folder', { folderId, name, userId, parentFolderId });
   const folder = await Folder.update(updateFields, {
     where: {
       id: folderId,
@@ -25,10 +34,75 @@ exports.updateFolder = async (folderId, name, userId, parentFolderId) => {
     },
   });
 
-  console.log('folder updated', folder);
   if (folder[0] === 0) {
     throw new Error('Folder not found');
   }
 
   return folder;
+};
+
+async function getDescendantFolderIds(folderId, userId, transaction) {
+  let folderIds = [folderId];
+
+  const subFolders = await Folder.findAll({
+    where: {
+      parent_folder_id: folderId,
+      user_id: userId
+    },
+    attributes: ['id'],
+    transaction
+  });
+
+  for (const subFolder of subFolders) {
+    const descendantIds = await getDescendantFolderIds(subFolder.id, userId, transaction);
+    folderIds = folderIds.concat(descendantIds);
+  }
+
+  return folderIds;
+}
+
+exports.markFolderAndContentsAsDeleted = async (folderId, userId) => {
+  const initialFolder = await Folder.findOne({
+    where: {
+      id: folderId,
+      user_id: userId,
+      deleted: false,
+    },
+  });
+
+  if (!initialFolder) {
+    throw new Error('Folder not found');
+  }
+
+  const t = await Folder.sequelize.transaction();
+
+  const folderIdsToMark = await getDescendantFolderIds(folderId, userId, t);
+
+  await Folder.update(
+    { deleted: true },
+    {
+      where: {
+        id: {
+          [Op.in]: folderIdsToMark,
+        },
+      },
+      transaction: t
+    }
+  );
+
+  await Note.update(
+    { deleted: true },
+    {
+      where: {
+        folder_id: {
+          [Op.in]: folderIdsToMark,
+        },
+        user_id: userId
+      },
+      transaction: t
+    }
+  );
+
+  await t.commit(); // Commit the transaction if everything is successful
+
 };
